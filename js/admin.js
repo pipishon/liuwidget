f6eb11ed18a3611c451cec10ef7a3da4d2d9f381
pipishon <pipishon@bk.ru>
2015-09-02 23:31:01 +0300

    home 2_09

--- js/admin.js
+++ js/admin.js
@@ -1,70 +1,41 @@
 
                 
-        // Prepare the variable that holds our custom media manager.
-        var liywidget_media_frame;
-       var clicked_element; 
+// Preare the variable that holds our custom media manager.
+var liywidget_media_frame;
+var clicked_element; 
         
-        // Bind to our click event in order to open up the new media experience.
-        jQuery(document).ready(function(){
-           // jQuery('.liywidget-frame-show').hide();
-            jQuery('.add-image-button').live('click',function(e){
+// Bind to our click event in order to open up the new media experience.
+jQuery(document).ready(function(){
+    jQuery('.add-image-button').live('click',function(e){
                 e.preventDefault();
-			clicked_element=jQuery(this);
-			 if ( liywidget_media_frame ) {
-			     liywidget_media_frame.open();
-			return;
-			}
-		    
-
-		    liywidget_media_frame = wp.media.frames.liywidget_media_frame = wp.media({
+		clicked_element=jQuery(this);
+		if ( liywidget_media_frame ) {
+		     liywidget_media_frame.open();
+		return;
+		}
+		liywidget_media_frame = wp.media.frames.liywidget_media_frame = wp.media({
 			className: 'media-frame liywidget-frame',
 			frame: 'select',
-                multiple: false,
-                title: 'title',
-                library: {
-                    type: 'image'
-                },
-                button: {
-                    text:  'Add'
-                }
-            });
+			multiple: false,
+			title: 'title',
+			library: {
+			    type: 'image'
+			},
+			button: {
+			    text:  'Add'
+			}
+		});
     
             liywidget_media_frame.on('select', function(){
                 var media_attachment, img_url, media_attachments;
-                
                 // Grab our attachment selection and construct a JSON representation of the model.
                 media_attachment = liywidget_media_frame.state().get('selection').first().toJSON();
-                
                 img_url = media_attachment.url;
-               clicked_element.html('<img src="'+img_url+'" />');
-//                jQuery('.liywidget-frame-show').after('<img id = "'+media_attachment.id+'" class="liywidget-img" src = "' + img_url + '" height="100px">');
-                //alert(media_attachment.id);
-
+		clicked_element.html('<img src="'+img_url+'" />');
                	clicked_element.parent().next().find('input[type=hidden]').val(media_attachment.id);
-		    //               jQuery('.img-ids').val(media_attachment.id);        
-                });
-             liywidget_media_frame.open();
             });
+	    
+             liywidget_media_frame.open();
+    });
            
-/*	jQuery('#liuwidget-table tr').each(function(){
-		if (jQuery(this).find('input[type=hidden]').val()!=''){
-			jQuery(this).find('a').html('<img src="'+wp_get_attachment_image_src(jQuery(this).find('input [type=hidden]').val())+'" />'); 
-	
-		}
-			//parent().find('a').html('<img src="'+wp_get_attachment_image_src(jQuery(this).find('input [type=hidden]').val())+'" />'); 
-		 
-		});*/
-        });
-        //$(document.body).on('click', , function(e){
-            // Prevent the default action from occuring.
-           // e.preventDefault();
-            
-            
-            
-            /*
-            // If the frame already exists, re-open it.
-           
-    
-            // Now that everything has been set, let's open up the frame.
-            liywidget_media_frame.open();*/
-       // });
+});
--- liywidget.php
+++ liywidget.php
@@ -28,145 +28,84 @@ class LIY_Widget_Friedns extends WP_Widget {
 
 		/** This filter is documented in wp-includes/default-widgets.php */
 		$title = apply_filters( 'widget_title', empty( $instance['title'] ) ? '' : $instance['title'], $instance, $this->id_base );
-
-		/**
-		 * Filter the content of the Text widget.
-		 *
-		 * @since 2.3.0
-		 *
-		 * @param string    $widget_text The widget content.
-		 * @param WP_Widget $instance    WP_Widget instance.
-		 */
-/*		$text = apply_filters( 'widget_text', empty( $instance['text'] ) ? '' : $instance['text'], $instance );
-		echo $args['before_widget'];
-		if ( ! empty( $title ) ) {
-			echo $args['before_title'] . $title . $args['after_title'];
-		} ?>
-			<div class="textwidget"><?php echo !empty( $instance['filter'] ) ? wpautop( $text ) : $text; ?></div>
-		<?php
-		echo $args['after_widget'];*/
-		echo $instance['img1'];
 		?>
-<table id="liuwidget-table">
-	<tr>
 
-		<td><a class="add-image-button" href="<?php $instance['link1'] ?>" id="link1">
-            			
-	<?php if ($instance['img1']==''){ ?>
-			
-	<?php } else { ?>
-		<img src="<?php echo wp_get_attachment_image_src($instance['img1'])[0];?>" />
-	<?php } ?>
-			</a>
-		</td>
-		<td>
-			<a href="<?php echo $instance['link1'] ?>">link</a>
-		</td>
-	</tr>
-	<tr>
-		<td><a class="add-image-button" href="<?php $instance['link2'] ?>" id="link2">
-			<?php if ($instance['img2']==''){ ?>
-					
-			<?php } else { ?>
-				<img src="<?php echo wp_get_attachment_image_src($instance['img2'])[0];?>" />
-			<?php } ?>
-		</a></td>
-		<td>
-			<a href="<?php echo $instance['link2'] ?>">link</a>
-		</td>
-	</tr>
-	<tr>
-		<td><a class="add-image-button" href="<?php $instance['link3'] ?>" id="link3">
-			<?php if ($instance['img3']==''){ ?>
-			
-			<?php } else { ?>
-				<img src="<?php echo wp_get_attachment_image_src($instance['img3'])[0];?>" />
-			<?php } ?>
-		</a></td>
-		<td>
-			<a href="<?php echo $instance['link3'] ?>">link</a>
-		</td>
-	</tr>
-</table>
-<?php
+		<table id="liuwidget-table">
+				<?php for ($i=0;$i<3;$i++): ?>
+			<tr>
+
+				<td><a class="add-image-button" href="<?php $instance['link'.$i] ?>" id="link1">
+						
+				<?php if ($instance['img'.$i]): ?>
+					<img src="<?php echo wp_get_attachment_image_src($instance['img'.$i])[0];?>" />
+				<?php endif ?>
+
+				</a>
+				</td>
+				<td>
+					<a href="<?php echo $instance['link'.$i] ?>"><?php echo $instance['label'.$i] ?></a>
+				</td>
+			</tr>
+				<?php endfor ?>
+		</table>
+
+	<?php
 	}
 
 	public function update( $new_instance, $old_instance ) {
 		$instance = $old_instance;
 		$instance['title'] = strip_tags($new_instance['title']);
-		$instance['link1'] = strip_tags($new_instance['link1']);
-		$instance['link2'] = strip_tags($new_instance['link2']);
-		$instance['link3'] = strip_tags($new_instance['link3']);
-		$instance['img1'] = $new_instance['img1'];
-		$instance['img2'] = $new_instance['img2'];
-		$instance['img3'] = $new_instance['img3'];
+		for ($i=0;$i<3;$i++){
+			$instance['link'.$i] = strip_tags($new_instance['link'.$i]);
+			$instance['img'.$i] = $new_instance['img'.$i];
+			$instance['label'.$i] = $new_instance['label'.$i];
+		}
 		return $instance;
 	}
 
 	public function form( $instance ) {
 		$instance = wp_parse_args( (array) $instance, array( 
+			'link0' => '', 
 			'link1' => '', 
 			'link2' => '', 
-			'link3' => '', 
+			'label0' => '', 
+			'label1' => '', 
+			'label2' => '', 
+			'img0' => '', 
 			'img1' => '', 
 			'img2' => '', 
-			'img3' => '', 
 			'title' => '', 
 		) );
 		$title = strip_tags($instance['title']);
-
-
-
-?>
+	?>
 		<p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?></label>
 		<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr($title); ?>" /></p>
 
-<table id="liuwidget-table">
-	<tr>
+		<table id="liuwidget-table">
+			<?php for ($i=0;$i<3;$i++): ?>
+			<tr>
 
-		<td><a class="add-image-button" href="#" id="link1">
-            			
-	<?php if ($instance['img1']==''){ ?>
-			Add image
-	<?php } else { ?>
-		<img src="<?php echo wp_get_attachment_image_src($instance['img1'])[0];?>" />
-	<?php } ?>
-			</a>
-		</td>
-		<td>
-			<input type="text" name="<?php echo $this->get_field_name('link1'); ?>" value="<?php echo esc_attr($instance['link1']); ?>" />
-			<input type="hidden" name="<?php echo $this->get_field_name('img1'); ?>" value="<?php echo esc_attr($instance['img1']); ?>">
-		</td>
-	</tr>
-	<tr>
-		<td><a class="add-image-button" href="#" id="link2">
-			<?php if ($instance['img2']==''){ ?>
+				<td><a class="add-image-button" href="#" id="link1">
+						
+			<?php if ($instance['img'.$i]==''): ?>
 					Add image
-			<?php } else { ?>
-				<img src="<?php echo wp_get_attachment_image_src($instance['img2'])[0];?>" />
-			<?php } ?>
-		</a></td>
-		<td>
-			<input type="text" name="<?php echo $this->get_field_name('link2'); ?>" value="<?php echo esc_attr($instance['link2']); ?>" >
-			<input type="hidden" name="<?php echo $this->get_field_name('img2'); ?>" value="<?php echo esc_attr($instance['img2']); ?>">
-		</td>
-	</tr>
-	<tr>
-		<td><a class="add-image-button" href="#" id="link3">
-			<?php if ($instance['img3']==''){ ?>
-					Add image
-			<?php } else { ?>
-				<img src="<?php echo wp_get_attachment_image_src($instance['img3'])[0];?>" />
-			<?php } ?>
-		</a></td>
-		<td>
-			<input type="text" name="<?php echo $this->get_field_name('link3'); ?>" value="<?php echo esc_attr($instance['link3']); ?>" >
-			<input type="hidden" name="<?php echo $this->get_field_name('img3'); ?>" value="<?php echo esc_attr($instance['img3']); ?>">
-		</td>
-	</tr>
-</table>
+			<?php  else:  ?>
+				<img src="<?php echo wp_get_attachment_image_src($instance['img'.$i])[0];?>" />
+			<?php endif ?>
+					</a>
+				</td>
+				<td>
+					Text
+					<input type="text" name="<?php echo $this->get_field_name('label'.$i); ?>" value="<?php echo esc_attr($instance['label'.$i]); ?>" /><br />
+					Link
+					<input type="text" name="<?php echo $this->get_field_name('link'.$i); ?>" value="<?php echo esc_attr($instance['link'.$i]); ?>" />
+					<input type="hidden" name="<?php echo $this->get_field_name('img'.$i); ?>" value="<?php echo esc_attr($instance['img'.$i]); ?>">
+				</td>
+			</tr>
+			<?php endfor?>
+		</table>
 
-<?php
+	<?php
 	}
 }
 
